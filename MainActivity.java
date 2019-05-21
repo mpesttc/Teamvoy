0b0490013526a8794cdfb1010dd88aba9dac7858
Maksym <maksim_p@ua.fm>
2019-05-21 22:25:04 +0300

    activity_main
    A JAVA code from the MainActivity.

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.testtask.sunrisesunset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
